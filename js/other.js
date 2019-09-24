2e95c228f1131def1f21b1590fbb6dbf801805fc
alyssa-vasquez <alyssa.n.vasquez@outlook.com>
2019-09-24 16:13:56 -0700

    finally

--- js/other.js
+++ js/other.js
@@ -1,3 +1,9 @@
-function flip() {
-    $('.card').toggleClass('flipped');
+function flip1() {
+  $(".card1").toggleClass("flipped");
+}
+function flip2() {
+  $(".card2").toggleClass("flipped");
+}
+function flip3() {
+  $(".card3").toggleClass("flipped");
 }
